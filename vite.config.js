3f8f3d06c61e753fd320d333970ce47619d72f23
Kejdi Kishta <155896977+KejdiKishta@users.noreply.github.com>
2024-06-07 15:40:50 +0200

    add: import sass in blade add: alias for resourses in config

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,11 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+
+    // alisa for resources
+    resolve: {
+        alias: {
+        '~resources': '/resources/'
+        }
+    },
 });
